e08da20ec01896975e829e23427a5eeb4551d346
tlantea23 <tlantea2306@gmail.com>
2026-06-16 23:00:29 +0530

    fix: enable api routes by removing static export

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export', // Hei hi uncomment/ti nung rawh. Play Store tan a pawimawh ber
+
   images: {
     unoptimized: true, // Capacitor tan a ngai
   },
-  trailingSlash: true, // 'export' tan true a tha zawk
+  trailingSlash: false, // 'export' tan true a tha zawk
 }
 
 export default nextConfig
